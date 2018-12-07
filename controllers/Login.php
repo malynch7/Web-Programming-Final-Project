70525d07b55a5a82518dded6a34c3f1fadf0b41b
Mark Lynch <malynch7@gmail.com>
2018-12-07 13:53:43 -0500

    MVC? ...Maybe???

--- controllers/Login.php
+++ controllers/Login.php
@@ -7,7 +7,7 @@ class Login extends Controller{
     public function __construct($model) {
         parent::__construct($model);
         $this->model = $model;
-        $this->model->template = 'views/Login.php';
+        $this->model->template = 'views/login.php';
     }
 
     public function clicked(){
