c3e364330e548e6d56e8e61cf850696a16367f70
garavsoni1234 <garavsoni12@gmail.com>
2018-12-07 12:46:51 -0500

    Register and Login

---
+++ Register/MainRegister.php
@@ -0,0 +1,54 @@
+<?php
+
+     $RegFirstName  = $_REQUEST['RegFirstName'];
+     $RegLastName   = $_REQUEST['RegLastName'];
+     $RegEmail      = $_REQUEST['RegEmail'];
+     $RegPassword   = $_REQUEST['RegPassword'];
+     $ReRegPassword = $_REQUEST['ReRegPassword'];
+     if($RegPassword == $ReRegPassword){
+        echo "works well";
+     }else{
+        echo "does not work well";
+     }
+     echo $RegFirstName;
+     echo $RegLastName;
+     echo  $RegDOB;
+     echo  $RegEmail;
+$servername = "localhost";
+$username = "gsoni1";
+$password = "gsoni1";
+$dbname = "gsoni1";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo 'successfully connected';
+}
+$sql = "SELECT email, password, first_name, last_name FROM user";
+$result = $conn->query($sql);
+if (!$result) {
+    trigger_error('Invalid query: ' . $conn->error);
+}
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> email: ".$row["email"]." - password: ".$row["password"]."first name" . $row["first_name"]." last name: ". $row["last_name"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+$sql = "INSERT INTO user (email, password, first_name, last_name)
+VALUES ('$RegEmail', '$RegPassword', '$RegFirstName', '$RegLastName')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
---
+++ Register/register.php
@@ -0,0 +1,34 @@
+
+<html>
+    <head>
+            <title>Register </title>
+             <link rel="stylesheet" type="text/css" href="LoginStyle.css">
+    </head>
+    <body>
+
+         <div class="CorrectLogin">
+                <img src="PhotoIcon.png" class="IconMain">
+                <h1>Register</h1>
+                 <form action="MainRegister.php"method="post">
+                <p>First Name: </p>
+                <input type="text" name="RegFirstName" id="RegFirstName">
+                <p>Last Name: </p>
+                <input type="text" name="RegLastName" id="RegLastName">
+                <p>Email: </p>
+                <input type="text" name="RegEmail" id="RegEmail">
+                <p>Password: </p>
+                <input type="password" name="RegPassword" id="RegPassword">
+                <p>Re-type Password: </p>
+                <input type="password" name="ReRegPassword" id="ReRegPassword">
+                <input type="submit" name="submitTwo" value="Login">
+                </form>
+
+
+
+
+            </form>
+
+
+        </div>
+    </body>
+    </html>
