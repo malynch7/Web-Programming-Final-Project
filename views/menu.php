dcc7f2c396850b36e3dc53d15987cff5f10fe174
Mark Lynch <malynch7@gmail.com>
2018-12-08 01:26:35 -0500

    polished login, registration, and menu

--- views/menu.php
+++ views/menu.php
@@ -10,19 +10,46 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="views/css/Style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
+<center>
+    <div class="divtop">
+        <h1>Cruise and Park</h1>
+        <ul class="ulnav">
+            <li class="linav"><a href="#about">YouTube Video</a></li>
+            <li class="linav"><a href="https://github.com/malynch7/Web-Programming-Final-Project">GitHub</a></li>
+            <li class="linav"><a href="index.php?action=logout">Log out</a></li>
+        </ul>
+    </div>
+</center>
 <br>
-<h1 align="center">Cruise and Park</h1>
 <br>
-<p align="center"><a class="btn btn-primary" href="index.php?action=startBooking" role="button">Book a New Trip</a></p>
-<br>
-<p align="center">Or</p>
-<br>
-<p align="center"><a class="btn btn-primary" href="index.php?controller=profile" role="button">View Previous
-        Bookings</a></p>
+<div class="card" style="width: 22rem; margin-top: 5%">
+    <div class="card-body">
+        <h3 class="card-title" align="center">Cruise and Park</h3>
+        <hr>
+        <br><br>
+        <p align="center"><a class="btn btn-primary" href="index.php?action=startBooking" role="button">Book a New
+                Trip</a></p>
+        <br>
+        <p align="center">Or</p>
+        <br>
+        <p align="center"><a class="btn btn-primary" href="index.php?controller=profile" role="button">View Previous
+                Bookings</a></p>
 
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<hr>
+<center><p>© Copyright 2018 Cruise and Park</p></center>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
