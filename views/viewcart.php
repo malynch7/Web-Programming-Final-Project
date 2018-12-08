2496e9c812bc0ed56a5cdd0ad54230bd159fc183
garavsoni1234 <garavsoni12@gmail.com>
2018-12-08 14:45:29 -0500

    Update viewcart.php

--- views/viewcart.php
+++ views/viewcart.php
@@ -5,25 +5,25 @@
     $ParkinglotMain=0;
     $ParkingSelec=$_COOKIE["Parking_Bool"];
     $ExEmail='garavsoni12';
-       $largestvalue =0;
-       $servername = "localhost";
-       $username = "gsoni1";
-       $password = "gsoni1";
-       $dbname = "gsoni1";
-
-$ParkingSelec =$ParkingSelec+0;
+    $largestvalue =0;
+    $servername = "localhost";
+    $username = "gsoni1";
+    $password = "gsoni1";
+    $dbname = "gsoni1";
+    $Num_of_Rooms=0;
+    $ParkingSelec =$ParkingSelec+0;
 
 $DestinationMain = $_COOKIE["Destination_Selection"] + $DestinationMain;
 $RoomMain = $Des_Cr[$_COOKIE["Room_Selection"]] + $RoomMain;
 $ParkinglotMain = $Des_Cr[$_COOKIE["Parking_Lot"]] + $ParkinglotMain;
 
 
+
 $Des_Cr = array(399.99, 599.99, 999.99);
 $Room_Sel = array(199.99, 299.99, 399.99);
 $Lot_Sel = array(25, 35, 60);
-$Total_amount = $Des_Cr[$_COOKIE["Destination_Selection"]] +  $Des_Cr[$_COOKIE["Room_Selection"]] + $Des_Cr[$_COOKIE["Parking_Lot"]];
-alert($_COOKIE["Destination_Selection"]);
-
+$Total_amount = $Des_Cr[$_COOKIE["Destination_Selection"]] +  $Room_Sel[$_COOKIE["Room_Selection"]] + $Lot_Sel[$_COOKIE["Parking_Lot"]];
+$_COOKIE["numberOfRooms"] = $_COOKIE["numberOfRooms"] * $Des_Cr[$_COOKIE["Destination_Selection"]];
 
 
 // Create connection
@@ -60,7 +60,7 @@ $largestvalue=$largestvalue+1;
 
 
 
-$sql = "INSERT INTO purchase(purchase_number, destination_code,room_tier, number_of_rooms,parking,parking_lot,total_price) VALUES($largestvalue,'$DestinationMain','$RoomMain',1,'$ParkingSelec','$Lot_Sel','$Total_amount')";
+$sql = "INSERT INTO purchase(purchase_number, destination_code,room_tier, number_of_rooms,parking,parking_lot,total_price) VALUES($largestvalue,'$DestinationMain','$RoomMain','$_COOKIE["numberOfRooms"]','$ParkingSelec','$Lot_Sel','$Total_amount')";
 
 
 
